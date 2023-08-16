3eade0b2d9bd2e364627e657e4ef73a2ea5ead94
farhanrauf27 <farhanrauf27@gmail.com>
2023-08-16 22:08:36 +0500

    16/8/23

--- s.js
+++ s.js
@@ -1,38 +1,48 @@
-// Task 1: Build a function-based console log message generator
-function consoleStyler(color,background,fontSize,txt) {
-    let message="%c" + txt;
-    let style=`color: ${color};`
-    style+=`background: ${background};`
-    style+=`font-size: ${fontSize};`
-    console.log(message,style)
-}
-
-
-// Task 2: Build another console log message generator
-function celebrateStyler(reason) {
-    let fontStyle="color: tomato; font-size: 50px";
-    if(`reason === "birthday"`){
-        console.log(`%cHappy birthday`, fontStyle);
+// Task 1: Code a Person class
+class Person {
+    constructor(name,age,energy){
+        this.name=name;
+        this.age=age;
+        this.energy=energy;
     }
-    else if(reason === "champions"){
-        console.log(`%cCongrats on the title!`, fontStyle);
+    name = "Tom"
+    age = 20
+    energy = 100
+ sleep(){
+    let x=this.energy+10
+    console.log("Increased Energy level is: "+x)
+}
+ doSomethingFun(){
+    let y=this.energy -10
+    console.log("Decrased Energy level is: " +y)
+}
+}
+// Task 2: Code a Worker class
+class Worker extends Person{
+    constructor(name,age,energy,xp,hourlyWage){
+        super()
+        this.xp=xp
+        this.hourlyWage=hourlyWage
     }
-    // console.log(message, style);
+    xp = 0
+    hourlyWage = 10;
+ goToWork(){
+    let c=this.xp+10
+    console.log("Increased xp is: "+c)
+}
+}
+// Task 3: Code an intern object, run methods
+function intern() {
+    let intern=new Worker("Bob",21,110,0,10)
+    intern.goToWork()
+    return intern
 }
 
-// Task 3: Run both the consoleStyler and the celebrateStyler functions
-consoleStyler(`'#1d5c63'`,`'#ede6db'`,`'40px'`,`'Congrats!'`)
-celebrateStyler(`'birthday'`)
-
-// Task 4: Insert a congratulatory and custom message
-function styleAndCelebrate(color, background, fontSize, txt, reason) {
-    consoleStyler(color,background,fontSize,txt);
-    celebrateStyler(reason);
+// Task 4: Code a manager object, methods
+function manager() {
+    let manager= new Worker("Alice",30,120,100,30)
+    manager.doSomethingFun()
+    return manager
+    
 }
-
-
-
-styleAndCelebrate(`'ef7c8e'`,`'fae8e0'`,`'30px`,`'You made it!'`,`'champions'`)
-// Call styleAndCelebrate
-
 
